614a7a42e85200b85cd95f81357bea553064eebd
Fauno Guazina <prof.guazina@gmail.com>
2023-11-02 12:36:40 +0100

    allow internet traffic with no secure internet address

--- M08_Act02_ConectWithXampp/app/src/main/java/com/faunog/m08_act02_conectwithxampp/MainActivity.java
+++ M08_Act02_ConectWithXampp/app/src/main/java/com/faunog/m08_act02_conectwithxampp/MainActivity.java
@@ -2,7 +2,9 @@ package com.faunog.m08_act02_conectwithxampp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Código para permitir tráfico de red no seguro en versiones posteriores a Android P
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
         connectVariableWithElements();
         applyListenersToButtonLogin();
     }
