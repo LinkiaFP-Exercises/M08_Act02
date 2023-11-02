0fb23946c8491d5c737327a80159d51941064cdf
Fauno Guazina <prof.guazina@gmail.com>
2023-11-02 13:59:25 +0100

    create new activity and implement a basic toobar

---
+++ M08_Act02_ConectWithXampp/app/src/main/java/com/faunog/m08_act02_conectwithxampp/DatabaseViewer.java
@@ -0,0 +1,30 @@
+package com.faunog.m08_act02_conectwithxampp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class DatabaseViewer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_viewer);
+
+        toolbarNavigationFunction();
+
+    }
+
+    private void toolbarNavigationFunction(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        toolbar.setNavigationOnClickListener(v -> finish());
+    }
+}
