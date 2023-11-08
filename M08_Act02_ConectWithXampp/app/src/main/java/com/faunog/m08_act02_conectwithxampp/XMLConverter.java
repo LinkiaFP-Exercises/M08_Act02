6dec7a0a40ca2f16de79a3a616b42d99ba488862
Fauno Guazina <prof.guazina@gmail.com>
2023-11-08 18:42:52 +0100

    refactoring catchStatusResponseFromXMLDocument

--- M08_Act02_ConectWithXampp/app/src/main/java/com/faunog/m08_act02_conectwithxampp/XMLConverter.java
+++ M08_Act02_ConectWithXampp/app/src/main/java/com/faunog/m08_act02_conectwithxampp/XMLConverter.java
@@ -2,6 +2,7 @@ package com.faunog.m08_act02_conectwithxampp;
 
 import android.util.Log;
 
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -30,12 +31,17 @@ public class XMLConverter {
         }
     }
 
-
     public static String catchStatusResponseFromXMLDocument(Document document) {
-        NodeList listaItem = (NodeList) document.getElementsByTagName("respuesta");
-        Element element = (Element) listaItem.item(0);
-        return element.getElementsByTagName("estado").item(0).getTextContent();
+        try {
+            NodeList listaItem = document.getElementsByTagName("respuesta");
+            Element element = (Element) listaItem.item(0);
+            return element.getElementsByTagName("estado").item(0).getTextContent();
+        } catch (NullPointerException | DOMException e) {
+            Log.e(TAG, "Error in catchStatusResponseFromXMLDocument:\n\n\n" + e.getMessage());
+            return null;
+        }
     }
+
 
     public static List<UsuariosMySQL> convertXMLtoUsuariosMySQL(Document document) {
         List<UsuariosMySQL> usuariosMySQLList = new ArrayList<>();
