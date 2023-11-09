74d13a2ab23d118150298366c562dfd4179f6385
Fauno Guazina <prof.guazina@gmail.com>
2023-11-09 11:07:48 +0100

    documentation of OpenActivities.java

--- M08_Act02_ConectWithXampp/app/src/main/java/com/faunog/m08_act02_conectwithxampp/OpenActivities.java
+++ M08_Act02_ConectWithXampp/app/src/main/java/com/faunog/m08_act02_conectwithxampp/OpenActivities.java
@@ -6,8 +6,22 @@ import android.content.Intent;
 
 import androidx.appcompat.widget.Toolbar;
 
+/**
+ * Clase que gestiona la apertura de diferentes actividades en la aplicación.
+ * Esta clase proporciona métodos para abrir actividades específicas y controlar la finalización de actividades anteriores.
+ *
+ * @author <a href="https://about.me/prof.guazina">Fauno Guazina</a>
+ * @version 1.1
+ * @since 18/10/2023
+ */
 public class OpenActivities {
 
+    /**
+     * Abre la actividad de visor de base de datos.
+     *
+     * @param context El contexto de la aplicación.
+     * @see OpenActivities#ifActivityFinish(Context)
+     */
     public static void databaseViewer(Context context) {
         ifActivityFinish(context);
         Intent intent = new Intent(context, DatabaseViewer.class);
@@ -15,6 +29,12 @@ public class OpenActivities {
         context.startActivity(intent);
     }
 
+    /**
+     * Abre la actividad de visor de intentos fallidos.
+     *
+     * @param context El contexto de la aplicación.
+     * @see OpenActivities#ifActivityFinish(Context)
+     */
     public static void failedAttemptsViewer(Context context) {
         ifActivityFinish(context);
         Intent intent = new Intent(context, FailedAttemptsViewer.class);
@@ -22,6 +42,12 @@ public class OpenActivities {
         context.startActivity(intent);
     }
 
+    /**
+     * Abre la actividad principal.
+     *
+     * @param context El contexto de la aplicación.
+     * @see OpenActivities#ifActivityFinish(Context)
+     */
     public static void mainViewer(Context context) {
         ifActivityFinish(context);
         Intent intent = new Intent(context, MainActivity.class);
@@ -29,6 +55,11 @@ public class OpenActivities {
         context.startActivity(intent);
     }
 
+    /**
+     * Finaliza la actividad actual si es una instancia de Activity.
+     *
+     * @param context El contexto de la aplicación.
+     */
     private static void ifActivityFinish(Context context) {
         if (context instanceof Activity) {
             Activity activity = (Activity) context;
@@ -36,6 +67,13 @@ public class OpenActivities {
         }
     }
 
+    /**
+     * Establece la funcionalidad de navegación del toolbar para ir al visor principal.
+     *
+     * @param toolbar El toolbar que se utilizará.
+     * @param context El contexto de la aplicación.
+     * @see OpenActivities#mainViewer(Context)
+     */
     public static void toolbarGoToMainViewer(Toolbar toolbar, Context context) {
         toolbar.setNavigationOnClickListener(v -> mainViewer(context));
     }
